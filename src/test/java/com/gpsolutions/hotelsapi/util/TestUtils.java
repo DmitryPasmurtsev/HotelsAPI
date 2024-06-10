aad9105a328cfeb66365957a397d67c3bd8998a4
DmitryPasmurtsev <dima427614@gmail.com>
2024-06-10 12:54:30 +0300

    formatting

--- src/test/java/com/gpsolutions/hotelsapi/util/TestUtils.java
+++ src/test/java/com/gpsolutions/hotelsapi/util/TestUtils.java
@@ -13,8 +13,6 @@ import com.gpsolutions.hotelsapi.entity.ArrivalTime;
 import com.gpsolutions.hotelsapi.entity.Contacts;
 import com.gpsolutions.hotelsapi.entity.Hotel;
 import lombok.experimental.UtilityClass;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -47,15 +45,6 @@ public class TestUtils {
     public static final String NEW_COUNTY = "NewCounty";
     public static final String DEFAULT_CHECK_IN = "14:00";
     public static final String DEFAULT_CHECK_OUT = "12:00";
-    public static final int VALID_PAGE = 0;
-    public static final int VALID_SIZE = 10;
-    public static final int VALID_TOTAL = 2;
-    public static final int INVALID_PAGE = -1;
-    public static final int INVALID_SIZE = -1;
-    public static final String INVALID_ORDER_BY = "xyz";
-    public static final String VALID_ORDER_BY = "id";
-    public static final boolean NOT_BLOCKED = false;
-    public static final boolean BLOCKED = true;
 
     public static Hotel getDefaultHotel() {
         return Hotel.builder()
@@ -109,17 +98,6 @@ public class TestUtils {
                 .contacts(getDefaultContactsDto())
                 .build();
     }
-
-//    public static Hotel getSecondHotel() {
-//        return Hotel.builder()
-//                .id(NEW_ID)
-//                .name(NEW_NAME)
-//                .surname(NEW_SURNAME)
-//                .email(NEW_EMAIL)
-//                .phone(NEW_PHONE)
-//                .build();
-//    }
-
 
     public static HotelDetailResponse getDefaultHotelDetailResponse() {
         return HotelDetailResponse.builder()
@@ -257,128 +235,4 @@ public class TestUtils {
     public static Set<String> getNewAmenitiesNames() {
         return new HashSet<>(Arrays.asList("CCC", "DDD"));
     }
-//
-//    public static PassengerResponse getDefaultPassengerResponse() {
-//        return PassengerResponse.builder()
-//                .id(DEFAULT_ID)
-//                .name(DEFAULT_NAME)
-//                .surname(DEFAULT_SURNAME)
-//                .email(DEFAULT_EMAIL)
-//                .phone(DEFAULT_PHONE)
-//                .build();
-//    }
-//
-//    public static PassengerResponse getSecondPassengerResponse() {
-//        return PassengerResponse.builder()
-//                .id(NEW_ID)
-//                .name(NEW_NAME)
-//                .surname(NEW_SURNAME)
-//                .email(NEW_EMAIL)
-//                .phone(NEW_PHONE)
-//                .build();
-//    }
-//
-//    public static Passenger getNotSavedPassengerEntity() {
-//        return Passenger.builder()
-//                .name(DEFAULT_NAME)
-//                .surname(DEFAULT_SURNAME)
-//                .email(DEFAULT_EMAIL)
-//                .phone(DEFAULT_PHONE)
-//                .build();
-//    }
-//
-//    public static RatingUpdateDto getRatingUpdateDto() {
-//        return RatingUpdateDto.builder()
-//                .userId(DEFAULT_ID)
-//                .rating(NEW_RATING)
-//                .build();
-//    }
-//
-//    public static List<Passenger> getEntityList() {
-//        return new ArrayList<>(Arrays.asList(
-//                getDefaultPassenger(),
-//                getSecondPassenger()
-//        ));
-//    }
-//
-//    public static Page<Passenger> getEntityPage() {
-//        return new PageImpl<>(Arrays.asList(
-//                getDefaultPassenger(),
-//                getSecondPassenger()),
-//                Pageable.ofSize(VALID_SIZE),
-//                VALID_TOTAL
-//        );
-//    }
-//
-//    public static List<PassengerResponse> getPassengerResponsesList() {
-//        return new ArrayList<>(Arrays.asList(
-//                getDefaultPassengerResponse(),
-//                getSecondPassengerResponse()
-//        ));
-//    }
-//
-//    public static PassengersListResponse getDefaultPassengersListResponse(List<PassengerResponse> passengers) {
-//        return PassengersListResponse.builder()
-//                .passengers(passengers)
-//                .size(passengers.size())
-//                .total(passengers.size())
-//                .build();
-//    }
-//
-//    public static PassengersListResponse getPassengersListResponseWithSort(List<PassengerResponse> passengers) {
-//        return PassengersListResponse.builder()
-//                .passengers(passengers)
-//                .size(passengers.size())
-//                .total(passengers.size())
-//                .sortedByField(VALID_ORDER_BY)
-//                .build();
-//    }
-//
-//    public static PassengersListResponse getPassengersListResponseWithPagination(List<PassengerResponse> passengers) {
-//        return PassengersListResponse.builder()
-//                .passengers(passengers)
-//                .size(passengers.size())
-//                .total(passengers.size())
-//                .page(VALID_PAGE)
-//                .build();
-//    }
-//
-//    public static PassengersListResponse getPassengersListResponseWithSortAndPagination(List<PassengerResponse> passengers) {
-//        return PassengersListResponse.builder()
-//                .passengers(passengers)
-//                .size(passengers.size())
-//                .total(passengers.size())
-//                .page(VALID_PAGE)
-//                .sortedByField(VALID_ORDER_BY)
-//                .build();
-//    }
-//
-//    public static PassengerCreationRequest getDefaultPassengerCreationRequest() {
-//        return PassengerCreationRequest.builder()
-//                .name(DEFAULT_NAME)
-//                .surname(DEFAULT_SURNAME)
-//                .email(DEFAULT_EMAIL)
-//                .phone(DEFAULT_PHONE)
-//                .build();
-//    }
-//
-//    public static PassengerCreationRequest getDefaultPassengerRequestForUpdate() {
-//        return PassengerCreationRequest.builder()
-//                .name(NEW_NAME)
-//                .surname(NEW_SURNAME)
-//                .email(NEW_EMAIL)
-//                .phone(NEW_PHONE)
-//                .build();
-//    }
-//
-//    public static PassengerResponse getUpdatedPassengerResponse() {
-//        return PassengerResponse.builder()
-//                .id(DEFAULT_ID)
-//                .name(NEW_NAME)
-//                .surname(NEW_SURNAME)
-//                .email(NEW_EMAIL)
-//                .phone(NEW_PHONE)
-//                .build();
-//    }
-
 }
